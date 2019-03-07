27ab03465db3c6800a6ba3098b4366bdd7478dd8
Amrit Pal Singh <amritpalsinghs369@gmail.com>
2019-03-06 18:01:03 -0800

    add points events

--- userprogress/admin.py
+++ userprogress/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from .models import UserWatchedVideo, UserAttemptedQuestion, TotalPoints, TotalCoins
+from .models import UserWatchedVideo, UserAttemptedQuestion, TotalPoints, TotalCoins, UserReadNotes
 
 
 
 admin.site.register(UserWatchedVideo)
+admin.site.register(UserReadNotes)
 admin.site.register(UserAttemptedQuestion)
 admin.site.register(TotalPoints)
 admin.site.register(TotalCoins)
