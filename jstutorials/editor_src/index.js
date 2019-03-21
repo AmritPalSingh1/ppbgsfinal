cab8c4defeac5c7cf8783f9169e68cf6417e105a
summerjacket <dxmcxd@gmail.com>
2019-03-21 14:52:54 -0700

    add fibonacci and factorial exercises

--- jstutorials/editor_src/index.js
+++ jstutorials/editor_src/index.js
@@ -26,7 +26,7 @@ import { toFormData } from './utils.js';
 
 store.dispatch(setCoins($('#coins').html()));
 store.dispatch(setHintsUsed($('#hints-used').html()));
-store.dispatch(fetchExercise('/static/exercises/cond_even_num.yaml'));
+store.dispatch(fetchExercise('/static/exercises/func_factorial_recursive.yaml'));
 
 // -- Handle code change --------------------------------------------
 
