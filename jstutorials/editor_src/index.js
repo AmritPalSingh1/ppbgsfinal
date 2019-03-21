319f3365affed0fbc007d9c050c1f252dee94939
summerjacket <dxmcxd@gmail.com>
2019-03-21 15:17:15 -0700

    fix flatten exercise regex

--- jstutorials/editor_src/index.js
+++ jstutorials/editor_src/index.js
@@ -26,7 +26,7 @@ import { toFormData } from './utils.js';
 
 store.dispatch(setCoins($('#coins').html()));
 store.dispatch(setHintsUsed($('#hints-used').html()));
-store.dispatch(fetchExercise('/static/exercises/func_factorial_recursive.yaml'));
+store.dispatch(fetchExercise('/static/exercises/array_flatten.yaml'));
 
 // -- Handle code change --------------------------------------------
 
