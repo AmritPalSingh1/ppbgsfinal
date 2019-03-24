f58105985cfeb3dcf5b6f91bcbd34868c0a160a7
summerjacket <dxmcxd@gmail.com>
2019-03-24 13:18:13 -0700

    add array rotate exercise

--- jstutorials/editor_src/modals.js
+++ jstutorials/editor_src/modals.js
@@ -1,13 +1,13 @@
+/* global $ */
+
+// do you feel sick?
+// because I definitely feel sick
+
 import {
   DEFAULT_INDENT,
   DEFAULT_KEYMAP,
   DEFAULT_THEME,
 } from './redux/constants';
-
-/* global $ */
-
-// do you feel sick?
-// because I definitely feel sick
 
 const createModal = (id, title, body, footer) => {
   $('#modal-container').append(
