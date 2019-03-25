d1125ccab4f762f7a3cc87a1581dfb98cddee91e
summerjacket <dxmcxd@gmail.com>
2019-03-25 14:52:53 -0700

    compile with babel

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,15 @@ module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
   entry: './jstutorials/editor_src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
   output: {
     filename: 'code_editor.js',
     path: path.resolve(__dirname, 'jstutorials/static/js'),
